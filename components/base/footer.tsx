03e51c28688733b1b94f0f397da89156cf542dc5
Neeha Ravula <ravulaneeha@gmail.com>
2025-12-30 12:23:03 -0800

    made year dynamic

--- components/base/footer.tsx
+++ components/base/footer.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="w-full flex items-center justify-between p-8 font-mono text-xs">
       <button>INFO</button>
-      <button>© 2025</button>
+      <button>© {currentYear}</button>
     </footer>
   );
 }
